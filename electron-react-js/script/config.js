6c00cf1825de0ceeae85d5d643436573d88286c5
lifl9 <lifl9@yonyou.com>
2018-07-11 14:39:32 +0800

    [electron-react-js] create

---
+++ electron-react-js/script/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  devServerPort: 6090,
+  websocketPort: 6091
+}
---
+++ electron-react-js/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className={'root'}>
+        <p>{this.state.text}</p>
+        <button onClick={this.click}>click</button>
+      </div>
+    )
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      text: 'React JavaScript'
+    }
+    this.click = this.click.bind(this)
+  }
+
+  click () {
+    this.setState((prevState) => {
+      return {
+        text: prevState.text + '!!!'
+      }
+    })
+  }
+}
+
+(async function () {
+  const ms = await (ms => new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms)
+    }, ms)
+  }))(1000)
+  console.log(ms)
+})()
+
+export default process.env.NODE_ENV !== 'production' ? require('react-hot-loader').hot(module)(App) : App
