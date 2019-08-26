6849f7038a958f46c6fa63310464c319f596a536
toyobayashi <lifenglin314@outlook.com>
2019-08-26 23:49:58 +0800

    electron-vue-ts

--- electron-vue-ts/app/src/main/ipc.ts
+++
@@ -1,9 +0,0 @@
-import './ipc-main'
-import { ipcMain } from 'electron'
-
-export default function init () {
-  ipcMain.handle('showArgs', (_e, ...args) => {
-    console.log(args)
-    return args
-  })
-}
--- electron-vue-ts/app/src/renderer/typings/vue.d.ts
+++
@@ -1,7 +0,0 @@
-import * as electron from 'electron'
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    electron: typeof electron
-  }
-}
